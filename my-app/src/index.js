f2c4bc5109b610270a8dc05a466fcad5d2b8b0aa
indonswe <afc2002afc@hotmail.com>
2021-11-15 11:36:15 +0100

    first commit

---
+++ my-app/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+// import bootstrap css
+import "bootstrap/dist/css/bootstrap.css"; 
+//import DataTable from "./components/DataTable";
+//import DemoRouter from "./components/DemoRouter";
+import Welcome from "./components/Welcome";
+
+ReactDOM.render(<Welcome />, document.getElementById("root"));
+//ReactDOM.render(<DataTable />, document.getElementById("root"));
